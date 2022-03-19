0632258f4b6f0bad614f3320c330eeb1093b8620
Katy Harrod <m.catherine.harrod@gmail.com>
2022-03-19 14:29:50 -0600

    All passing tests

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -12,6 +12,7 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
+    result = @hash[:copyright]
 
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
@@ -19,12 +20,25 @@ class NytimesTest < Minitest::Test
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
 
+    result = @hash[:results]
+
     assert result.is_a? (Array)
     assert_equal 44, result.count
   end
 
   def test_it_can_get_all_stories_with_subsection_of_politics
+
+    # result (an array result.last[:title]) result last = hash
+
+    # @data = Nytimes::DATA
+
+    result = @hash[:results].filter_map { |element| element if element[:subsection] == "Politics" }
+    # an array of hashes
     #Set your code to the local variable, "result"
+
+
+    # results where section is US
+    # subsection is politics
 
 
     assert result.is_a? (Array)
@@ -34,5 +48,9 @@ class NytimesTest < Minitest::Test
     assert_equal "Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.",
       result.last[:title]
   end
+  #
+  # test = NytimesTest.new
+  # test.setup
+
 
 end
